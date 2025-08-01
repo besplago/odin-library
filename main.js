11f7206529d915f3b326f4fd1fa404a2e081b651
besplago <bruce_esplago@hotmail.com>
2025-08-01 13:39:24 +0200

    refac: renderer class

--- main.js
+++ main.js
@@ -8,13 +8,32 @@ class Book {
   }
 }
 
-class Library {
+class Observable {
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, callback) {
+    if (!this.listeners[event]) this.listeners[event] = [];
+    this.listeners[event].push(callback);
+  }
+
+  emit(event, ...args) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach((cb) => cb(...args));
+    }
+  }
+}
+
+class Library extends Observable {
   constructor(books) {
+    super();
     this.books = books;
   }
 
   addBook(book) {
     this.books.push(book);
+    this.emit("libraryUpdated");
   }
 
   removeBook(bookId) {
@@ -22,6 +41,7 @@ class Library {
     if (index !== -1) {
       this.books.splice(index, 1);
     }
+    this.emit("libraryUpdated");
   }
 
   updateReadStatus(bookId, readStatus) {
@@ -29,6 +49,105 @@ class Library {
     if (book) {
       book.read = readStatus;
     }
+    this.emit("libraryUpdated");
+  }
+}
+
+class Renderer {
+  constructor(library) {
+    this.library = library;
+  }
+
+  createBookCard(book) {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.dataset.id = book.id;
+
+    const topRow = document.createElement("div");
+    topRow.className = "card-top-row";
+
+    const textContainer = document.createElement("div");
+    const title = document.createElement("h3");
+    title.textContent = book.title.toUpperCase();
+    const author = document.createElement("p");
+    author.textContent = book.author;
+
+    textContainer.appendChild(title);
+    textContainer.appendChild(author);
+
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.classList.add("icon", "x-icon", "remove-card-icon");
+    svg.setAttribute("viewBox", "0 0 16 16");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("role", "button");
+    svg.setAttribute("aria-label", "Remove book");
+    svg.setAttribute("tabindex", "0");
+
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute(
+      "d",
+      "M5.1716 8.00003L1.08582 3.91424L3.91424 1.08582L8.00003 5.1716L12.0858 1.08582L14.9142 3.91424L10.8285 8.00003L14.9142 12.0858L12.0858 14.9142L8.00003 10.8285L3.91424 14.9142L1.08582 12.0858L5.1716 8.00003Z"
+    );
+
+    svg.appendChild(path);
+
+    svg.addEventListener("click", () => {
+      const removeEvent = new CustomEvent("removeBook", {
+        bubbles: true,
+        detail: { bookId: book.id },
+      });
+      card.dispatchEvent(removeEvent);
+    });
+
+    topRow.appendChild(textContainer);
+    topRow.appendChild(svg);
+
+    const bookStats = document.createElement("div");
+    bookStats.className = "book-stats";
+
+    const pages = document.createElement("span");
+    pages.textContent = `${book.pages} PAGES`;
+
+    const readStatus = document.createElement("div");
+    readStatus.className = "read-status";
+
+    const label = document.createElement("label");
+    label.htmlFor = `read-status-${book.id}`;
+    const labelText = document.createElement("span");
+    labelText.textContent = "READ";
+    label.appendChild(labelText);
+
+    const checkbox = document.createElement("input");
+    checkbox.id = `read-status-${book.id}`;
+    checkbox.type = "checkbox";
+    checkbox.name = "read-status";
+    checkbox.checked = book.read;
+    checkbox.addEventListener("change", () => {
+      const removeEvent = new CustomEvent("updateReadStatus", {
+        bubbles: true,
+        detail: { bookId: book.id, read: book.read },
+      });
+      card.dispatchEvent(removeEvent);
+    });
+
+    readStatus.appendChild(label);
+    readStatus.appendChild(checkbox);
+
+    bookStats.appendChild(pages);
+    bookStats.appendChild(readStatus);
+
+    card.appendChild(topRow);
+    card.appendChild(bookStats);
+
+    return card;
+  }
+
+  displayBooks() {
+    const cardGrid = document.getElementById("card-grid");
+    this.library.books.forEach((book) => {
+      cardGrid.appendChild(this.createBookCard(book));
+    });
   }
 }
 
@@ -45,91 +164,6 @@ function updateStats() {
   document.getElementById("total-books").textContent = totalBooks;
   document.getElementById("pages-read").textContent = pagesRead;
   document.getElementById("total-pages").textContent = totalPages;
-}
-
-function createBookCard(book) {
-  const card = document.createElement("div");
-  card.className = "card";
-
-  card.dataset.id = book.id;
-
-  const topRow = document.createElement("div");
-  topRow.className = "card-top-row";
-
-  const textContainer = document.createElement("div");
-  const title = document.createElement("h3");
-  title.textContent = book.title.toUpperCase();
-  const author = document.createElement("p");
-  author.textContent = book.author;
-
-  textContainer.appendChild(title);
-  textContainer.appendChild(author);
-
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.classList.add("icon", "x-icon", "remove-card-icon");
-  svg.setAttribute("viewBox", "0 0 16 16");
-  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  svg.setAttribute("role", "button");
-  svg.setAttribute("aria-label", "Remove book");
-  svg.setAttribute("tabindex", "0");
-
-  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path.setAttribute(
-    "d",
-    "M5.1716 8.00003L1.08582 3.91424L3.91424 1.08582L8.00003 5.1716L12.0858 1.08582L14.9142 3.91424L10.8285 8.00003L14.9142 12.0858L12.0858 14.9142L8.00003 10.8285L3.91424 14.9142L1.08582 12.0858L5.1716 8.00003Z"
-  );
-
-  svg.appendChild(path);
-
-  svg.addEventListener("click", () => {
-    const removeEvent = new CustomEvent("removeBook", {
-      bubbles: true,
-      detail: { bookId: book.id },
-    });
-    card.dispatchEvent(removeEvent);
-  });
-
-  topRow.appendChild(textContainer);
-  topRow.appendChild(svg);
-
-  const bookStats = document.createElement("div");
-  bookStats.className = "book-stats";
-
-  const pages = document.createElement("span");
-  pages.textContent = `${book.pages} PAGES`;
-
-  const readStatus = document.createElement("div");
-  readStatus.className = "read-status";
-
-  const label = document.createElement("label");
-  label.htmlFor = `read-status-${book.id}`;
-  const labelText = document.createElement("span");
-  labelText.textContent = "READ";
-  label.appendChild(labelText);
-
-  const checkbox = document.createElement("input");
-  checkbox.id = `read-status-${book.id}`;
-  checkbox.type = "checkbox";
-  checkbox.name = "read-status";
-  checkbox.checked = book.read;
-  checkbox.addEventListener("change", () => {
-    const removeEvent = new CustomEvent("updateReadStatus", {
-      bubbles: true,
-      detail: { bookId: book.id, read: book.read },
-    });
-    card.dispatchEvent(removeEvent);
-  });
-
-  readStatus.appendChild(label);
-  readStatus.appendChild(checkbox);
-
-  bookStats.appendChild(pages);
-  bookStats.appendChild(readStatus);
-
-  card.appendChild(topRow);
-  card.appendChild(bookStats);
-
-  return card;
 }
 
 function setupEventListeners(library) {
@@ -200,14 +234,11 @@ function setupEventListeners(library) {
   });
 }
 
-function displayBooks(library) {
-  const cardGrid = document.getElementById("card-grid");
-  library.forEach((book) => {
-    cardGrid.appendChild(createBookCard(book));
-  });
-}
-
 const myLibrary = new Library([]);
+const displayer = new Renderer(myLibrary);
+myLibrary.on("libraryUpdated", () => {
+  displayer.displayBooks();
+});
 
 myLibrary.addBook(
   new Book("Vertical Leap for Dummies", "Dr. Air Jordan", 290, true)
@@ -249,8 +280,5 @@ myLibrary.addBook(
   new Book("The GOAT Debate: Why Bron > MJ", "King James Press", 311, true)
 );
 
-console.log(myLibrary.books);
-
-displayBooks(myLibrary);
 setupEventListeners(myLibrary);
 updateStats();
